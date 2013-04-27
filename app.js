33a0402ca4aa2435a930dc1805464a55b14b9a8f
zefanja@freenet.de <info@zefanjas.de>
2013-04-27 15:16:57 +0700

    * remove old code from app.js

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-var zlib = require('zlib');
-var zip = require('zip');
-var unzip = require("unzip");
-var fs = require('fs');
-var async = require('async');
-
-var mappings = require("./lib/mappings.js");
 var SWMgr = require("./lib/SWModuleMgr.js");
-
-var fileName = "esv/nt.bzz";
-var ntIndex = "esv/nt.bzs";
-var ntIndexV = "esv/nt.bzv";
 
 SWMgr.getModule("gerneue", function (inModule) {
     inModule.renderText("Neh 1");
