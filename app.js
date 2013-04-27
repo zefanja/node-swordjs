91044bca726f1eddc80a5845fc1c1d692c553a95
zefanja@freenet.de <info@zefanjas.de>
2013-04-27 15:20:19 +0700

    * added callback in renderText()

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var SWMgr = require("./lib/SWModuleMgr.js");
 
 SWMgr.getModule("gerneue", function (inModule) {
-    inModule.renderText("Neh 1");
+    inModule.renderText("Neh 1", function (inText) {
+        console.log(inText);
+    });
 });
