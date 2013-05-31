fdcaeef7f5582102dfd84297dc3ec87e5ced9619
zefanja <zefanja@freenet.de>
2013-05-31 16:24:43 +0700

    * merge patch from mcepl

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var SWMgr = require("./lib/SWModuleMgr.js");
 
-
-SWMgr.getModule("esv", function (inModule) {
+SWMgr.getModule("gerneue", function (inModule) {
     if (inModule)
-        inModule.renderText("Mt 1:1", {footnotes: true}, function (inText) {
+        inModule.renderText("Mt 1:1", {footnotes: false}, function (inText) {
             console.log(inText);
         });
 });
